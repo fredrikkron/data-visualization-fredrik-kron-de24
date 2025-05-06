59d6d9c5e007d40246414178fef9f861808a0da8
fredrikkron <fkron95@gmail.com>
2025-05-06 11:24:50 +0200

    filtered with slider

--- code_alongs/09_taipy_dashboard/main.py
+++ code_alongs/09_taipy_dashboard/main.py
@@ -2,6 +2,8 @@ import taipy.gui.builder as tgb
 from taipy.gui import Gui
 import pandas as pd
 from utils.constants import DATA_DIRECTORY
+from frontend.charts import create_municipality_bar
+
 
 df = pd.read_excel(
     DATA_DIRECTORY / "resultat-ansokningsomgang-2024.xlsx",
@@ -10,6 +12,37 @@ df = pd.read_excel(
 )
 
 
+def filter_df_municipality(df, educational_area="Data/IT"):
+    return (
+        df.query("Utbildningsområde == @educational_area")["Kommun"]
+        .value_counts()
+        .reset_index()
+        .rename({"count": "Ansökta utbildningar"}, axis=1)
+    )
+
+
+def filter_data(state):
+    print(state)
+    df_municipality = filter_df_municipality(state.df)
+
+    state.municipality_chart = create_municipality_bar(
+        df_municipality.head(state.number_municipalities),
+        xlabel="# ANSÖKTA UTBILDNINGAR",
+        ylabel="KOMMUN",
+    )
+
+
+number_municipalities = 5
+
+df_municipality = filter_df_municipality(df)
+
+municipality_chart = create_municipality_bar(
+    df_municipality, xlabel="# ANSÖKTA UTBILDNINGAR", ylabel="KOMMUN"
+)
+
+# --------------------------------------------------------------
+# --------------------------------------------------------------
+
 with tgb.Page() as page:
     with tgb.part(class_name="container card"):
         tgb.text("# MYH dashboard 2024", mode="md")
@@ -17,9 +50,20 @@ with tgb.Page() as page:
         with tgb.layout(columns="2 1"):
             with tgb.part(class_name="card"):
                 tgb.text("Graph")
+                tgb.chart(figure="{municipality_chart}")
 
             with tgb.part(class_name="card"):
-                tgb.text("Filters")
+                tgb.text("## Filtrera data", mode="md")
+                tgb.text("Filtrera antalet kommuner", mode="md")
+
+                tgb.slider(
+                    "{number_municipalities}",
+                    min=5,
+                    max=len(df_municipality),
+                    continuous=False,
+                )
+
+                tgb.button("FILTRERA DATA", class_name="plain", on_action=filter_data)
 
         with tgb.part(class_name="card"):
             tgb.text("Raw data")
@@ -27,4 +71,4 @@ with tgb.Page() as page:
 
 
 if __name__ == "__main__":
-    Gui(page).run(use_reloader=True, port=8080)
+    Gui(page).run(use_reloader=False, port=8080)
